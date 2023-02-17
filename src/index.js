8389c5dc65f900ae3b4dcc3b41a5c8488dd7385e
Dennis Every <dennis@ansyra.com>
2023-02-17 09:57:06 -0400

    Add webpack

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+root.render(<h1>Hello from React application</h1>);
